3c5be87f7dc6701a14ad0a31ed9ed12e0f4ead0a
Danny200523 <danicrack0709@gmail.com>
2025-09-28 18:46:04 -0500

    feat: :rocket: finish commit

--- script/inicioUser.js
+++ script/inicioUser.js
@@ -1,5 +1,6 @@
 const URL_PEL = 'http://62.169.28.169/movies/all-pel'
 const URL_MPOP = 'http://62.169.28.169/movies/pel-pop'
+const URL_SEARCH = 'http://62.169.28.169/movies/search/'
 
 const slides = document.querySelector(".slides");
 const dots = document.querySelectorAll(".dot");
@@ -36,30 +37,58 @@ const carrusel = document.querySelector(".carrusel");
 const prevp = document.querySelector(".prevPopular");
 const nextp = document.querySelector(".nextPopular");
 
-function popMore(){
+async function popMore(){
   fetch(URL_MPOP)
   .then(Response=>Response.json())
   .then(data=>{
     let pp = ``
     for ( let i = 0; i < 20; i++ ){
       pp += `
-      <div class="pelicula">
+      <div data-movie-id="${data[i]._id}" class="pelicula">
       <img src="${data[i].poster}" alt="">
       <p class="titulo">${data[i].title}</p>
       <p class="año">${data[i].year}</p>
       </div>
       `
     }
-    console.log(pp)
     carrusel.innerHTML = pp
     const peliculas = document.querySelectorAll(".pelicula");
     peliculas.forEach(pelicula => {
       pelicula.addEventListener("click", () => {
+        const id = pelicula.dataset.movieId;
         modalReviews.style.display = "none"; 
         modalGeneral.style.display = "block";
+        fetch(`${URL_SEARCH}${id}`)
+        .then(Response=>Response.json())
+        .then(data=>{
+          console.log(data)
+          const datas = data[0]
+          const main = document.querySelector(".infomain")
+          main.innerHTML = `
+          <p class="titulo">${datas.title}</p>
+          <p class="summary">
+            ${datas.summary}
+          </p>
+          `;
+          const detail = document.querySelector(".details")
+          detail.innerHTML = `
+          <div class="yearcontainer">
+            <p>Category</p>
+            <p>${datas.genres}</p>
+          </div>
+          <div class="categorycontainer">
+            <p>Year</p>
+            <p>${datas.year}</p>
+          </div>
+          <div class="popularitycontainer">
+            <p>Popularity</p>
+            <p>${datas.popularity}</p>
+          </div>
+          `;
+          const imgEl = document.querySelector('#modalGeneral .img img').src = datas.backdrop;
+        })
       });
     });
-    
   })
 }
 
