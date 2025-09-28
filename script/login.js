d40df9e448e36190c900c6993bf065cdbef4decf
Sarasofia1214 <dasaro1206@gmail.com>
2025-09-28 02:26:29 -0500

    feat: :sprakles: Finishing the login

---
+++ script/login.js
@@ -0,0 +1,54 @@
+// Selección de elementos
+const loginBtn = document.querySelector(".BotonIniciar");
+const emailInput = document.querySelector(".User");
+const passwordInput = document.querySelector(".Password");
+
+const API_URL = "http://62.169.28.169/auth/login";
+
+loginBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  const email = emailInput.value.trim();
+  const password = passwordInput.value.trim();
+
+  if (!email || !password) {
+    alert("Por favor, ingresa el usuario y la contraseña.");
+    return;
+  }
+
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "accept": "application/json"
+      },
+      body: JSON.stringify({
+        usuario: email,
+        contrasena: password
+      })
+    });
+
+    const data = await response.json();
+    const token = data[0]?.token;
+    const isAdmin = data[1]?.admin;
+
+    if (response.ok && token) {
+      localStorage.setItem("token", token);
+      localStorage.setItem("isAdmin", isAdmin); 
+      
+      if (isAdmin) {
+        window.location.href = "../html/inicioAdmin.html";
+      } else {
+        window.location.href = "../html/inicioUser.html";
+      }
+
+    } else {
+      alert("Usuario o contraseña incorrectos ❌");
+    }
+
+  } catch (error) {
+    console.error("Error de conexión:", error);
+    alert("No se pudo conectar con el servidor 🚨");
+  }
+});
